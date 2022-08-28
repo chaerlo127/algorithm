d1bdd4058208f944b26a21e1fdb963a0a06e47e9
chaerlo127 <monynony0203@gmail.com>
2022-08-28 23:56:23 +0900

    2022-08-28

---
+++ sliver/_11050.java
@@ -0,0 +1,30 @@
+package sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11050 {
+
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			System.out.println(factorial(n)/(factorial(k)*factorial(n-k)));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static int factorial(int a) {
+		if(a <= 1) return 1;
+		else return a*factorial(a-1);
+	}
+
+}
