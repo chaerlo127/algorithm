6e0743d8d96200727fd21dfc1fa793ad851f8af6
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-08-13 16:58:02 +0900

    Update 일곱 난쟁이.py

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,20 +1,27 @@
 num = 9
-array = []
-for i in range(num):
-    array.append(int(input()))
-
+lists = [int(input()) for _ in range(num)]
 a = 0
 b = 0
-for i in range(num-1):
-  for j in range(i+1, num):
-    if sum(array)-(array[i]+array[j])==100:
-      a=array[i]
-      b=array[j]
 
+# 리스트를 살펴 보며, 합이 100이 되는 경우 break
+for i in range(num - 1):
+    for j in range(i+1, num):
+        if sum(lists) - lists[i] - lists[j] == 100:
+            a = lists[i]
+            b = lists[j]
+            break
+    
+# 이렇게하면 답이 안나옴 왜인지 모르겠음        
+# for i in range(num - 1):
+#     for j in range(i+1, num):
+#         a = lists[i]
+#         b = lists[j]
+#         if sum(lists) - a - b == 100:
+#             break
 
-array.remove(a)
-array.remove(b)
-array.sort()
+lists.remove(a)
+lists.remove(b)
+lists.sort()
 
-for i in range(len(array)):
-    print(array[i]) 
+for i in lists:
+    print(i)
