8496899e49d93e6d489f756beb27b1a41a8f7bc2
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-08-24 15:50:25 +0900

    과거 코드 -> 현재 코드로 변경

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,23 +1,18 @@
 import sys
 
-# 백트래킹
-N, M = map(int, sys.stdin.readline().split())
-s = []
-visited = [False for _ in range(N + 1)]
+n, m = map(int, sys.stdin.readline().split())
+l = [] # 답을 저장하기 위한 리스트
 
 
-def DFS():
-    if len(s) == M:
-        print(' '.join(map(str, s)))
+def back():
+    if len(l) == m: # 리스트의 길이가 m과 같다면 print (답을 출력)
+        print(" ".join(map(str, l)))
         return
-    for i in range(1, N + 1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        s.append(i)
-        DFS()  # 재귀로 값을 다 구하면
-        visited[i] = False  # 원래 있던 상태로 변경
-        s.pop()
+    for i in range(1, n + 1): # 1 ~ n 까지의 자연수 중에서
+        if i not in l: # 자연수가 리스트 내에 저장이 되어 있지 않다면 => 중복을 해결하기 위함
+            l.append(i) # 리스트 뒤에 저장
+            back() # 재귀
+            l.pop() # 리스트 내 삭제 => 다른 경우의 수를 위해
 
 
-DFS()
+back()
