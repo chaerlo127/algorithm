0ac88b9e3d897ba3c33a4f66ae4a36b847954ac6
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-09-07 17:25:04 +0900

    스택 0907 새로 푼 코드

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -21,3 +21,27 @@ for _ in range(N):
         if len(stack) == 0:
             print(-1)
         else : print(stack[-1])
+# 0907 새로 푼 코드
+# import sys
+#
+# n = int(sys.stdin.readline())
+# l = []
+#
+# for _ in range(n):
+#     s = sys.stdin.readline().split()
+#     if s[0] == 'push':
+#         l.append(s[1])
+#     if s[0] == 'top':
+#         print(l[-1])
+#     if s[0] == 'pop':
+#         if len(l) == 0:
+#             print(-1)
+#         else:
+#             print(l.pop())
+#     if s[0] == 'size':
+#         print(len(l))
+#     if s[0] == 'empty':
+#         if len(l) == 0:
+#             print(1)
+#         else:
+#             print(0)
