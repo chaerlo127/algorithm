822fab326c4ef8ae553d37eb2a7547ab52b37734
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-09-11 20:27:16 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+q = deque(enumerate(map(int, sys.stdin.readline().split())))
+ans = []
+
+while len(q) != 0:
+    index, val = q.popleft() # 가장 앞에 있는 index 값을 뽑아냄
+    ans.append(index+1) # 기존의 인덱스는 print 하고자 하는 값보다 1이 작게 설정되어 있음
+    if val > 0:
+        q.rotate(-(val-1))  # 오른쪽으로 이동 (이 때 가장 앞에 있는 값을 지웠기 때문에 -1을 하여 index의 값을 맞추고 이동
+    else:
+        q.rotate(-val) # 왼쪽으로 이동
+
+print(*ans)
