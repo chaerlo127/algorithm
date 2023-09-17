4327a13411e82d2093d70fe419fe9d789d6a334b
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-09-17 23:38:40 +0900

    Update 바이러스.py 주석 추가

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,19 @@
 import sys
 
-n = int(sys.stdin.readline())
-f = int(sys.stdin.readline())
+n = int(sys.stdin.readline()) # 컴퓨터 수
+f = int(sys.stdin.readline()) # 네트워크 상에서 연결되어 있는 수
 
-l = [[] for _ in range(n + 1)]
-visited = [0] * (n + 1)
-answer = 0
+l = [[] for _ in range(n + 1)] # 그래프 표시
+visited = [0] * (n + 1) # 방문 컴퓨터 표시
+answer = 0 # 정답
 
+# 그래프 표시
 for i in range(f):
     x, y = map(int, sys.stdin.readline().split())
     l[x].append(y)
     l[y].append(x)
 
-
+# 깊이 우선 탐색으로 문제 해결
 def dfs(k):
     global answer
     if not visited[k]:
