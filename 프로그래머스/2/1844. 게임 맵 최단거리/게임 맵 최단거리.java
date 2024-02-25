37e21f122bd2f2126b80c7a4715ff25eebbfdf5b
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2024-02-25 09:16:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.18 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 class Solution {
-    public int[] dx = {1, 0, -1, 0};
-    public int[] dy = {0, 1, 0, -1};
-    public int[][] visited;
-    
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
+    int[][] visited;
     public int solution(int[][] maps) {
         visited = new int[maps.length][maps[0].length];
         bfs(maps);
@@ -19,24 +18,25 @@ class Solution {
         queue.add(new int[]{x, y});
         
         while(!queue.isEmpty()){
-            int[] check = queue.remove();
-            x = check[0];
-            y = check[1];
+            int[] c = queue.poll();
+            x = c[0];
+            y = c[1];
             
-            // 상하좌우 살펴보기
-            for (int i = 0; i < 4 ; i++){
-                int cx = x + dx[i];
-                int cy = y + dy[i];
-                // 밖을 벗어나거나 가로막혀져 있는 경우
-                if(cx > maps.length - 1 || cx < 0 || cy > maps[0].length - 1 || cy < 0) continue;
+            for (int i = 0 ; i < 4 ; i++){
+                int cx = dx[i] + x;
+                int cy = dy[i] + y;
                 
-                // 방문한 이력이 없고 가로막혀져 있지 않은 경우
+                // 양옆이 막혀있거나, 밖으로 벗어난 경우
+                if(cx < 0 || cy < 0 || cx > maps.length -1 || cy > maps[0].length -1){
+                    continue;
+                }
+                
+                // visited 되어 있지 않은 경우 + 가로막혀져잇지 않은 경우 
                 if(visited[cx][cy] == 0 && maps[cx][cy] == 1){
                     visited[cx][cy] = visited[x][y] + 1;
                     queue.add(new int[]{cx, cy});
                 }
             }
         }
-        
     }
 }
